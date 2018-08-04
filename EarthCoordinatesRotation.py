d4ce991e18147929c578a4c47851524414d377ab
mhghasemi <mohosghasemi75@gmail.com>
2018-08-04 20:51:50 +0430

    EarthCoordinatesRotation.py and FileLoader.py comments added and ready for use

--- EarthCoordinatesRotation.py
+++ EarthCoordinatesRotation.py
@@ -5,9 +5,20 @@ import math
 import os
 from FileLoader import all2one
 
+"""
+This code merges all the .csv data files into one and then rotates them so that the data is in the north flat direction
+You can see it works fine when the Gravity data is all changed in (0,0,9.8) after the normalization is done
+
+--> Use the all2one_normalized function to do the job
+The path input parameter in the function is the relative path of the .csv data
+"""
+
+
+# Normalizes the data according to the data in the three columns of 'RotationVectorX', 'RotationVectorY',
+# 'RotationVectorZ', 'RotationVectorcos'
 
 def normalization(df):
-    rotation_vector = df[['Milliseconds','RotationVectorX', 'RotationVectorY', 'RotationVectorZ', 'RotationVectorcos']]
+    rotation_vector = df[['Milliseconds', 'RotationVectorX', 'RotationVectorY', 'RotationVectorZ', 'RotationVectorcos']]
 
     sensors = ['AccelerometerLinear', 'Accelerometer', 'Gravity', 'Compass', 'Gyroscope']
     headers = df.columns
@@ -23,21 +34,22 @@ def normalization(df):
 
         rotation_matrix = tf.axangle2mat(axis, angle)
         for sensor in df_sensors:
-            sensor_data = df.ix[row][[sensor+'X', sensor+'Y', sensor+'Z']]
-            normalized_row = np.matmul(rotation_matrix , np.asarray(sensor_data))
+            sensor_data = df.ix[row][[sensor + 'X', sensor + 'Y', sensor + 'Z']]
+            normalized_row = np.matmul(rotation_matrix, np.asarray(sensor_data))
             df.ix[row][sensor + 'X', sensor + 'Y', sensor + 'Z'] = normalized_row
 
     return df
+
 
 def all2one_normalized(path):
     df = all2one(path)
     return normalization(df)
 
 
-if __name__ == '__main__':
-
+if __name__ == '__main__':  # for testing the function
     from FileLoader import all2one
+
     path = 'data/WalkCarClustering'
-    df = all2one_normalized(path+'/Walk')
+    df = all2one_normalized(path + '/Walk')
     normalization(df)
-    df.to_csv(path+'/Walk/Normalized.csv',sep=',',index=False)
+    df.to_csv(path + '/Walk/Normalized.csv', sep=',', index=False)
--- FileLoader.py
+++ FileLoader.py
@@ -7,22 +7,28 @@ import sys
 import glob
 import errno
 
+"""
+This python code merges all the .csv files into one single .csv file for further easier use
+--> use the function all2one to do the job 
+note that this function is already used in EarthCoordinatesRotation.py which does all the things in one place
+"""
 
-def all2one(path):
+
+def all2one(path):  # here is the path of the data (the .csv files)
     result = pd.DataFrame
     flag = True
     for file in os.listdir(path):
         if not (os.path.isdir(path + '/' + file) and file == 'Normalized.csv'):
             df = pd.read_csv(path + '/' + file)
             newColumns = []
-            df['Milliseconds'] = np.floor(df['Milliseconds']//25)*25
+            df['Milliseconds'] = np.floor(df['Milliseconds'] // 25) * 25
             for column in df.columns:
                 if column != 'Milliseconds':
                     column = file[:-4] + column
                 newColumns.append(column)
             df.columns = newColumns
             # print(df.head())
-            if(flag):
+            if (flag):
                 result = df.drop_duplicates()
                 flag = False
             else:
@@ -30,7 +36,6 @@ def all2one(path):
     return result
 
 
-if __name__ == '__main__':
-
+if __name__ == '__main__':  # for testing the code
     df = all2one('./data/NormalizationTest')
     print(df.head())
