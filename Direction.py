cffbeb31170fd5f17dbe0dda3d675b60935e7a45
mhghasemi <mohosghasemi75@gmail.com>
2018-08-04 19:02:01 +0430

    Speed, Direction vector and compass bearing added with comments

--- Direction.py
+++ Direction.py
@@ -1,57 +1,91 @@
-from Rotation_Test import *
+from math import sin, cos, sqrt, atan2, radians
+import compassbearing as cb
+from EarthCoordinatesRotation import *
 from FileLoader import *
 import numpy as np
 import pandas as pd
 import transforms3d as tf
+import pyproj as proj
+
+"""In this piece of code the four fields ['Speed' , 'DirectionX' ,'DirectionY' , 'CompassBearing'] will be added to 
+the data frame 
+
+Where CompassBearing is the angle of the movement relative to the North pole (in degrees)
+and DirectionX and DirectionY are the values of the movement vectors (normalized)
+
+--> Use the add_direction function to do the job
+"""
 
 
-def get_lat_long(df):
-    return df[['Milliseconds', 'GPSLatitude', 'GPSLongitude']]
+# returns the distance of two points in KMs
+def get_distance(d_lat1, d_long1, d_lat2, d_long2):
+    # this part is another algorithm for testing the answer of the funcion
+    # dx = (d_long2 - d_long1) * 40042.74 * math.cos((d_lat1 + d_lat2) * math.pi / 360) / 360
+    # dy = (d_lat1 - d_lat2) * 40042.74 / 360
+    # result = {'dx' : dx , 'dy' : dy}
+    # print(result)
+    # print('distance with first solution is: ' , sqrt(dx**2 + dy**2))
 
-def get_distance(lat1,lat2,long1,long2):
-    R = 6378.1  # Radius of the Earth
-    brng = 1.57  # Bearing is 90 degrees converted to radians.
-    d = 15  # Distance in km
+    R = 6371.0  # Earth radius in KMs
 
-    lat1 = math.radians(52.20472)  # Current lat point converted to radians
-    lon1 = math.radians(0.14056)  # Current long point converted to radians
+    lat1 = radians(d_lat1)
+    lon1 = radians(d_long1)
+    lat2 = radians(d_lat2)
+    lon2 = radians(d_long2)
 
-    lat2 = math.asin(math.sin(lat1) * math.cos(d / R) +
-                     math.cos(lat1) * math.sin(d / R) * math.cos(brng))
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
 
-    lon2 = lon1 + math.atan2(math.sin(brng) * math.sin(d / R) * math.cos(lat1),
-                             math.cos(d / R) - math.sin(lat1) * math.sin(lat2))
+    # here are some mathematical calculations that i don't know why they work but they do! :))
+    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
 
-    lat2 = math.degrees(lat2)
-    lon2 = math.degrees(lon2)
+    distance = R * c
 
-def get_direction(df2):
-    df = df2.drop_duplicates(subset = 'GPSLatitude')
-    lat_dif_series = pd.Series()
-    long_dif_series = pd.Series()
+    return distance
+
+def add_direction(input_df):
+    df = input_df[['Milliseconds', 'GPSLatitude', 'GPSLongitude']]
+    df = df.drop_duplicates(subset='GPSLatitude')
+    compassbearing_series = pd.Series()
+    direction_x_series = pd.Series()
+    direction_y_series = pd.Series()
     speed_series = pd.Series()
     df = df.reset_index()
-    for row in range(0, len(df) - 1):
-        if row == len(df) - 2:
-            lat_dif_series.append(lat_dif_series.iloc[-1])
-            long_dif_series.append(long_dif_series.iloc[-1])
-            speed_series.append(speed_series.iloc[-1])
+    for row in range(0, len(df)):
+        if row == len(df) - 1 or np.isnan(df.loc[row, 'GPSLatitude']) or np.isnan(df.loc[row + 1, 'GPSLatitude']):
+            direction_x_series = direction_x_series.append(pd.Series(direction_x_series.iloc[-1]), ignore_index=True)
+            direction_y_series = direction_y_series.append(pd.Series(direction_y_series.iloc[-1]), ignore_index=True)
+            speed_series = speed_series.append(pd.Series(speed_series.iloc[-1]), ignore_index=True)
+            compassbearing_series = compassbearing_series.append(pd.Series(compassbearing_series.iloc[-1]),
+                                                                 ignore_index=True)
         else:
-            lat_dif = df.loc[row + 1, 'GPSLatitude'] - df.loc[row, 'GPSLatitude']
-            long_dif = df.loc[row + 1, 'GPSLongitude'] - df.loc[row, 'GPSLongitude']
-            lat_dif_series = lat_dif_series.append(pd.Series(lat_dif),ignore_index=True)
-            long_dif_series = long_dif_series.append(pd.Series(long_dif),ignore_index=True)
-            distance = np.sqrt(lat_dif**2 + long_dif**2)
-            speed_series = speed_series.append(pd.Series(distance / (df.loc[row+1 , 'Milliseconds'] - df.loc[row , 'Milliseconds'])),
-                                               ignore_index=True)
-    df = df.assign(Speed = speed_series.values)
-    df = df.assign(LatitudeMove = lat_dif_series.values )
-    df = df.assign(LongitudeMove = long_dif_series.values)
-    return df
+
+            lat1, long1 = df.loc[row, 'GPSLatitude'], df.loc[row, 'GPSLongitude']
+            lat2, long2 = df.loc[row + 1, 'GPSLatitude'], df.loc[row + 1, 'GPSLongitude']
+
+            distance = get_distance(lat1, long1, lat2, long2) * 1000
+            diff_timestamp = (df.loc[row + 1, 'Milliseconds'] - df.loc[row, 'Milliseconds']) / 1000
+
+            x, y, compassbearing = cb.calculate_initial_compass_bearing(lat1, long1, lat2, long2)
+
+            direction_x_series = direction_x_series.append(pd.Series(x), ignore_index=True)
+            direction_y_series = direction_y_series.append(pd.Series(y), ignore_index=True)
+            speed_series = speed_series.append(pd.Series(distance / diff_timestamp), ignore_index=True)
+            compassbearing_series = compassbearing_series.append(pd.Series(compassbearing), ignore_index=True)
+
+    df = df.assign(Speed=speed_series.values)
+    df = df.assign(DirectionX=direction_x_series.values)
+    df = df.assign(DirectionY=direction_y_series.values)
+    df = df.assign(CompassBearing=compassbearing_series.values)
+    df = df.drop(columns=['index', 'Milliseconds'])
+    input_df = pd.merge(input_df, df, how='left', on=['GPSLatitude', 'GPSLongitude'])
+
+    return input_df
 
 
-if __name__ == '__main__':
-    path = 'data/MoveDirectionNormalization/Normalized.csv'
-    df = pd.read_csv(path,engine = 'python')
-    Directions = get_direction(get_lat_long(df))
+if __name__ == '__main__':  # for testing the function
+    path = 'data/WalkCarClustering/Car/CarNormalized.csv'
+    df = pd.read_csv(path, engine='python')
+    Directions = add_direction(df)
     print(Directions.head())
